bdbe254e1a1a892c92e0a9877f6ce8279190aa77
Tor Livar Flugsrud <torlivar@torlivar.net>
2014-10-18 13:43:30 +0200

    Reorgnize api code

--- test.js
+++ test.js
@@ -2,13 +2,13 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/boldsite_test');
 
-var api = require('./model/EveXmlApi');
+var api = require('./lib/eveApi');
 debugger;
 
-api.eveApi('eve/CharacterInfo.xml.aspx', {characterID:94331649}, function(err, result) {
-  if (err) return console.log("Error: " + err);
+api.getPublicCharacterInfo(94449671, function(err, result) {
+  if (err) return console.log(err);
   debugger;
-  return console.log(JSON.stringify(result));
+  console.log("Character: " + result.characterName);
+  console.log("Corp: " + result.corporation);
+
 });
-
-
