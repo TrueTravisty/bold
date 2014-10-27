9d0806889bb8d6fb7b7b27dcc39a2136e2782e52
Tor Livar Flugsrud <torlivar@torlivar.net>
2014-10-27 23:07:34 +0100

    Start work on zbk api usage

--- test.js
+++ test.js
@@ -2,13 +2,10 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/boldsite_test');
 
-var api = require('./lib/eveApi');
+var api = require('./lib/zkbApi');
 ;
 
-api.getPublicCharacterInfo(94449671, function(err, result) {
+api.getData('killID/41880193/', function(err, result) {
   if (err) return console.log(err);
-  ;
-  console.log("Character: " + result.characterName);
-  console.log("Corp: " + result.corporation);
-
+  console.log(JSON.stringify(result));
 });
