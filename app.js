12185e6f909f1b194bcc3e7ec2ce72c317ad302e
Tor Livar Flugsrud <torlivar@torlivar.net>
2015-11-24 14:16:08 +0100

    Update modules

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ try{
 
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
