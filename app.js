1da27994d2053065b944a029f10f6c560943cba0
Tor Livar Flugsrud <torlivar@torlivar.net>
2015-11-28 10:01:03 +0100

    Update Angular

--- app.js
+++ app.js
@@ -75,6 +75,9 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'bold.ico')));
 app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/javascripts/', express.static(path.join(__dirname, 'node_modules/angular')));
+app.use('/javascripts/', express.static(path.join(__dirname, 'node_modules/angular-route')));
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
