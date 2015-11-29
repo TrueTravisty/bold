d81eb8d5c0233446f15b72739d001f158f9f0e75
Tor Livar Flugsrud <torlivar@torlivar.net>
2015-11-29 10:03:58 +0100

    Fix navbar and notices

--- public/javascripts/bold-ang.js
+++ public/javascripts/bold-ang.js
@@ -2,6 +2,10 @@
 (function() {
 var app = angular.module('bold', ['ui.bootstrap', 'ngTouch']);
 
+app.controller('MainCtrl', ['$scope', function($scope) {
+    
+}])
+
 app.controller('FrontPageCarouselCtrl', ['$scope', '$http', function ($scope, $http) {
   $scope.myInterval = 5000;
   $scope.noWrapSlides = false;
