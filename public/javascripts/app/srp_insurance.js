c05679c0e755c6ca2aef27eca2cce1d667bcc240
Tor Livar Flugsrud <torlivar@torlivar.net>
2016-01-24 00:09:39 +0100

    Make insurance table

--- public/javascripts/app/srp_insurance.js
+++ public/javascripts/app/srp_insurance.js
@@ -16,4 +16,99 @@ app.controller('InsuranceCtrl', ['$scope','$http', function($scope, $http) {
     })
 }]);
 
+app.directive('contenteditable', function($sce, $http) {
+    return {
+        require: 'ngModel',
+        scope: {
+            ship: "=",
+            field: "@"
+        },
+        link: function(scope, elm, attrs, ctrl) {
+            elm.bind('blur', function() {
+                // want to cancel when clicking outside
+                ctrl.$rollbackViewValue();                
+            });
+            
+            //ctrl.$commitViewValue = function() {
+            //    console.log("Setting ship " + scope.ship.typeName + " field " + scope.field + " to " + ctrl.$viewValue)
+           // }
+            
+ 
+            // model -> view
+            ctrl.$render = function(value) {
+                elm.html(ctrl.$viewValue);
+            };
+            
+            var formatFunction = function(value) {
+                if (attrs.allowtext) return value;
+                if (!value) return '';
+                var format = '0,0.00';
+                if (attrs.format) format = attrs.format;
+                var v = numeral(value).format(format);
+                return v;
+            }
+            
+            var parseFunction = function(value) {
+                if (attrs.allowtext) return value;
+                var a = numeral().unformat(value);
+                return a;
+            }
+            
+            ctrl.$parsers.push(parseFunction);
+            ctrl.$formatters.push(formatFunction);            
+        
+            
+            ctrl.$viewChangeListeners.push(function() {
+                if (!scope.ship || !scope.field) return;
+                
+                scope.ship[scope.field] = ctrl.$modelValue;
+                var id = scope.ship.typeID;
+                $http.put('/corpsrp/ship/' + id, {ship: scope.ship}).then(function(result) {                    
+                    scope.ship = result.data;
+                    ctrl.$modelValue = 'foo'; // triggers format pipeline
+                }, function(result) {
+                    // error code - read ship again to get the real value updated
+                    $http.get('/corpsrp/ship/' + id).then(function(result) {
+                        scope.ship = result.data;
+                    });
+                });
+            })
+            
+            elm.bind('keydown', function(event) {
+                console.log("keydown " + event.which);
+                var esc = event.which == 27,
+                    el = event.target,
+                    enterOrTab = event.which == 13 || event.which == 9;
+                    
+                if (!attrs.allowtext && !(
+                    (event.which >= 48 && event.which <= 57) ||  // digit 
+                    event.which == 8 || // backspace
+                    event.which == 46 || // delete
+                    (event.which >= 37 && event.which <= 40) ||  // arrows
+                    event.which == 9 || // tab
+                    event.which == 190 || event.which == 188 || event.which == 110))  // . or , 
+                    {
+                        event.preventDefault();
+                    }
+                    
+                if (enterOrTab) {
+                    console.log("enter");
+                    ctrl.$setViewValue(elm.html());
+                    el.blur();
+                    event.preventDefault();        
+                }
+
+                if (esc) {
+                        console.log("esc");
+                        ctrl.$rollbackViewValue();
+                        el.blur();
+                        event.preventDefault();                        
+                    }
+                    
+            });
+            
+        }
+    };
+});
+
 })();
--- routes/srp.js
+++ routes/srp.js
@@ -25,4 +25,37 @@ router.get('/insurance', restrictSrpManager, function(req, res, next) {
    return res.render('srp/insurance', { current: 'srp' }) 
 });
 
+router.param('id', function(req, res, next, id) {
+    seat.getShipInfo(id, function(err, value) {
+        if (err) return next(err);
+        if (!value || value.length != 1) return next('Wrong number of ships found')
+        req.ship = value[0];
+        next();
+    })
+})
+
+router.get('/ship/:id', restrictSrpManager, function(req, res, next) {
+    return res.json(req.ship);
+})
+
+router.put('/ship/:id', restrictSrpManager, function(req,res,next) {
+    if (!req.body.ship || req.body.ship.typeID != req.ship.typeID) {
+        res.status(404);
+        return res.json({error: "Ship not found or invalid typeID"});
+    }
+    seat.updateInsuranceInfo(req.body.ship.typeID, req.body.ship.notes, 
+        req.body.ship.insurancePrice, req.body.ship.insurancePayout, req.body.ship.srpPercentage, function(err) {
+            if (err) {
+                res.status(500); 
+                return res.json({error: "Could not update ship", info: err});
+            }
+            seat.getShipInfo(req.body.ship.typeID, function(err, value) {
+                if (err) return next(err);
+                if (!value || value.length != 1) return next('Wrong number of ships found')
+                var ship = value[0];                
+                res.json(ship);                
+            })            
+        })
+});
+
 module.exports = router;
